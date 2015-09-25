99d3857c2caacf9712a4cf67d58461c96e32b398
YingjieFan <davidjarkent@gmail.com>
2015-09-25 14:52:04 -0500

    Dynamic Resizing
    Make the whole drawing resizing according to the panel size. Make the
    margin dynamic also.

--- src/hw2_2/UIPanel.java
+++ src/hw2_2/UIPanel.java
@@ -1,9 +1,11 @@
 package hw2_2;
 
+import java.awt.Color;
 import java.awt.Event;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
@@ -18,6 +20,7 @@ import javax.swing.JPanel;
 import org.w3c.dom.css.Rect;
 
 class UIPanel extends JPanel implements MouseMotionListener,MouseListener{
+	private static final int PanelSizeToMarginRaito=8;
 	private int panelWidth;
 	private int panelHeight;
 	private int pxPerUnit;
@@ -64,8 +67,9 @@ class UIPanel extends JPanel implements MouseMotionListener,MouseListener{
 		}
 		sectionMainWidth = pxPerUnit*5;
 		sectionMainHeight = pxPerUnit*10;
-		sectionMainLeftMargin=200;
-		sectionMainTopMargin=100;
+
+		sectionMainLeftMargin=this.getWidth()/PanelSizeToMarginRaito;
+		sectionMainTopMargin=this.getHeight()/PanelSizeToMarginRaito;
 		middleGap = pxPerUnit;
 		sectionNextShapeX = sectionMainLeftMargin+sectionMainWidth+middleGap;
 		sectionNextShapeY = sectionMainTopMargin;
@@ -98,9 +102,12 @@ class UIPanel extends JPanel implements MouseMotionListener,MouseListener{
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
-		System.out.println(this.getHeight());
+		
 		panelHeight=this.getHeight();
 		panelWidth = this.getWidth();
+		sectionMainLeftMargin=this.getWidth()/PanelSizeToMarginRaito;
+		sectionMainTopMargin=this.getHeight()/PanelSizeToMarginRaito;
+		middleGap = pxPerUnit;
 		if(panelHeight>panelWidth){
 			pxPerUnit=panelWidth/12;
 		}else{
@@ -108,49 +115,82 @@ class UIPanel extends JPanel implements MouseMotionListener,MouseListener{
 		}
 		g2d = (Graphics2D)g;
 		drawSectionMainArea(g2d);
-		//drawSectionNextShape(g2d);
-		//drawSectionText(g2d);
-		//drawQuitButton(g2d);
+		drawSectionNextShape(g2d);
+		drawSectionText(g2d);
+		drawQuitButton(g2d);
 		
 		
 	}
 	
 	//The method for drawing the main game play section on the left including the Pause 
 	private void drawSectionMainArea(Graphics2D g2d){
-		sectionMainLeftMargin=pxPerUnit;
-		sectionMainTopMargin=pxPerUnit;
+
+		sectionMainWidth = pxPerUnit*5;
+		sectionMainHeight = pxPerUnit*10;
+		//System.out.println(this.getHeight()+","+this.getWidth());
 		Rectangle sectionMainRect = new Rectangle(sectionMainLeftMargin, sectionMainTopMargin, sectionMainWidth, sectionMainHeight);
 		g2d.draw(sectionMainRect);
+		
+		pauseRectWidth = sectionMainWidth/2;
+		pauseRectHeight = sectionMainHeight/9;
+		pauseRectX = sectionMainLeftMargin + sectionMainWidth/4;
+		pauseRectY = sectionMainTopMargin + 4*pauseRectHeight;
+		pauseTextX = pauseRectX;
+		pauseTextY = pauseRectY;
 		if(shouldPause == true){
 			Rectangle pauseRect = new Rectangle(pauseRectX,pauseRectY,pauseRectWidth,pauseRectHeight);
 			g2d.draw(pauseRect);
-			g2d.setFont(new Font("TimesRoman", Font.PLAIN, 20));
-			g2d.drawString(pauseText, pauseTextX+pauseRectWidth/3, pauseTextY+pauseRectHeight/2);
+			g2d.setFont(new Font("TimesRoman", Font.PLAIN, sectionNextShapeHeight/7*2));
+			g2d.drawString(pauseText, pauseTextX+pauseRectWidth/5, pauseTextY+pauseRectHeight/3*2);
 		}
+		repaint();
 	}
 	
 	private void drawSectionNextShape(Graphics2D g2d){
+		sectionNextShapeX = sectionMainLeftMargin+sectionMainWidth+middleGap;
+		sectionNextShapeY = sectionMainTopMargin;
+		sectionNextShapeWidth = sectionMainWidth*2/3;
+		sectionNextShapeHeight = sectionMainHeight/5;
 		Rectangle sectionNextShapeRect = new Rectangle(sectionNextShapeX, sectionNextShapeY, sectionNextShapeWidth, sectionNextShapeHeight);
 		g2d.draw(sectionNextShapeRect);
-		
+		int[] x = {sectionNextShapeX+sectionNextShapeWidth/10,sectionNextShapeX+sectionNextShapeWidth/2,sectionNextShapeX+sectionNextShapeWidth-sectionNextShapeWidth/10};
+		int[] y = {sectionNextShapeY+sectionNextShapeHeight-sectionNextShapeHeight/10,sectionNextShapeY+sectionNextShapeHeight/10,sectionNextShapeY+sectionNextShapeHeight-sectionNextShapeHeight/10};
+		Polygon poly = new Polygon(x,y,3);
+		g2d.drawPolygon(poly);
+		g2d.setColor(Color.YELLOW);
+		g2d.fillPolygon(poly);
+		g2d.setColor(Color.BLACK);
+		repaint();
+
 	}
 	
 	private void drawQuitButton(Graphics2D g2d){
-		
+		quitRectWidth = sectionNextShapeWidth/2;
+		quitRectHeight = sectionNextShapeHeight/2;
+		quitRectX = sectionNextShapeX;
+		quitRectY = sectionMainTopMargin+sectionMainHeight-quitRectHeight;
 		Rectangle rectQuit = new Rectangle(quitRectX, quitRectY, quitRectWidth, quitRectHeight);
-		g2d.drawString(quitText,quitRectX+quitRectWidth/3,quitRectY+quitRectHeight/2);
+		g2d.setFont(new Font("TimesRoman", Font.PLAIN, quitRectHeight/5*2));
+		g2d.drawString(quitText,quitRectX+quitRectWidth/4,quitRectY+quitRectHeight/3*2);
 		g2d.draw(rectQuit);
-		
+		repaint();
+
 	}
 
 	private void drawSectionText(Graphics2D g2d){
-		g2d.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+		textDisplay = "Level:  "+level+"\n"+"Lines:  "+lines+"\n"+"Score:  "+score;
+		textDisplayX = sectionNextShapeX;
+		textDisplayY = sectionNextShapeY+sectionNextShapeHeight*2;
+		System.out.println(sectionNextShapeWidth);
+
+		g2d.setFont(new Font("TimesRoman", Font.PLAIN, sectionNextShapeWidth/7));
 		int currentY=textDisplayY;
 		for (String line : textDisplay.split("\n")){
 			g2d.drawString(line, textDisplayX, currentY);
 			currentY += g2d.getFontMetrics().getHeight()*2;
 		}
-            
+		repaint();
+    
 	}
 	/**
 	 * 
